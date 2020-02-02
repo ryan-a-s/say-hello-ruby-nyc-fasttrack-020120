4aa99e3516df07d0dd2692fa37210ca64d3caf25
Ryan Seit <ryan@ryanseit.com>
2020-02-02 03:46:02 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,14 @@
 # Build your say_hello method here
+
+puts "What is your name?"
+full_name = gets.chomp
+
+def say_hello(name)
+  if name.empty? == false
+    puts "Hello #{name}!"
+  else
+    puts "Hello Ruby Programmer!"
+  end
+end
+
+say_hello(full_name)
